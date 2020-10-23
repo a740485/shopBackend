dddf7e5d26830f89f2238dde436d55ab946e137c
傅文皓 <a740485@gmail.com>
2020-10-23 13:51:46 +0800

    更新資料庫

--- shop.sql
+++ shop.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Oct 13, 2020 at 03:38 AM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+-- 主機： 127.0.0.1
+-- 產生時間： 
+-- 伺服器版本： 10.1.38-MariaDB
+-- PHP 版本： 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shop`
+-- 資料庫： `shop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cart`
+-- 資料表結構 `cart`
 --
 
 CREATE TABLE `cart` (
@@ -33,42 +35,45 @@ CREATE TABLE `cart` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `cart`
+-- 傾印資料表的資料 `cart`
 --
 
 INSERT INTO `cart` (`user_id`, `product_id`, `create_at`) VALUES
 ('1', '5', '2020-10-08 06:34:49'),
+('1', '6', '2020-10-23 05:35:28'),
 ('1', '7', '2020-10-08 06:22:04');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- 資料表結構 `product`
 --
 
 CREATE TABLE `product` (
   `id` int(20) NOT NULL,
   `user_id` int(20) NOT NULL,
   `title` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `img` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `img` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `price` int(5) DEFAULT NULL,
   `amount` int(5) DEFAULT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `product`
+-- 傾印資料表的資料 `product`
 --
 
 INSERT INTO `product` (`id`, `user_id`, `title`, `img`, `price`, `amount`, `update_time`) VALUES
-(5, 1, '7-11 海底撈 台灣製 懶人火鍋 自煮火鍋 火鍋 自熱火鍋 火鍋控 麻辣鍋 麻辣火鍋 火鍋湯底 小火鍋 懶人鍋', 'https://picsum.photos/188/188/?random=3', 100, 10, '2020-10-01 06:23:05'),
-(6, 1, '現貨新升級摺疊收納椅 收納椅 收納凳 收納箱 儲物箱 沙發 超耐重', 'https://picsum.photos/188/188/?random=4', 50, 40, '2020-10-02 06:16:07'),
-(7, 1, '【DIFF】韓版寬鬆可愛保暖毛毛絨小熊大學t 長袖上衣 長袖t恤 衣服 寬鬆上衣 素T 素色 冬天 帽t【W160】', 'https://picsum.photos/188/188/?random=5', 150, 50, '2020-10-02 06:16:22');
+(5, 1, 'MINI液晶行動電源 10000mAh 迷你行動電源 方便攜帶 智能晶片 大容量小體積 LED數據 【蝦皮團購】', 'https://cf.shopee.tw/file/4e93e07490f24b76d625a15d830a8992_tn', 100, 10, '2020-10-18 07:28:26'),
+(6, 1, '【買到戀愛】韓系文青ins純色V領外搭針織背心【M8121】', 'https://cf.shopee.tw/file/1b5295ed91100c4c29d629af25186b24_tn', 50, 40, '2020-10-18 07:26:18'),
+(7, 1, '7-11 海底撈 台灣製 懶人火鍋 自煮火鍋 火鍋 自熱火鍋 火鍋控 麻辣鍋 麻辣火鍋 火鍋湯底 小火鍋 懶人鍋', 'https://cf.shopee.tw/file/87ba50b5c6a67f524b6ee1609138f46b_tn', 150, 50, '2020-10-18 07:27:16'),
+(8, 2, '【全賣場最低 附發票】簡約地毯 客廳 沙發 茶几毯 卧室 家用 地毯 床邊毯 毛地毯 地毯 地墊 北歐風地毯 墊子', 'https://cf.shopee.tw/file/8260fa55d1f4770dfd52126fc6e5d711_tn', 300, 2, '2020-10-18 07:31:15'),
+(9, 1, '7-11 海底撈 台灣製', 'https://cf.shopee.tw/file/8260fa55d1f4770dfd52126fc6e5d711_tn', 499, 100, '2020-10-23 05:09:06');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- 資料表結構 `user`
 --
 
 CREATE TABLE `user` (
@@ -83,50 +88,51 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `user`
+-- 傾印資料表的資料 `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `phone`, `email`, `address`, `create_at`, `update_at`) VALUES
 (1, '1', '1', '', '', '', '2020-10-01 02:52:18', '2020-10-01 02:52:18');
 
 --
--- Indexes for dumped tables
+-- 已傾印資料表的索引
 --
 
 --
--- Indexes for table `cart`
+-- 資料表索引 `cart`
 --
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`user_id`,`product_id`);
 
 --
--- Indexes for table `product`
+-- 資料表索引 `product`
 --
 ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- 資料表索引 `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在傾印的資料表使用自動增長(AUTO_INCREMENT)
 --
 
 --
--- AUTO_INCREMENT for table `product`
+-- 使用資料表自動增長(AUTO_INCREMENT) `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `user`
+-- 使用資料表自動增長(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
